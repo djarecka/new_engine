24a077f7e82c99a57c9825146017bf512338474d
Dorota Jarecka <djarecka@gmail.com>
2017-01-24 08:40:34 -0500

    new mapper patterns work, still not everything

--- super_node.py
+++ super_node.py
@@ -1,35 +1,118 @@
 import re
 import numpy as np
+import pdb
 
 
 class SNode(object):
     def __init__(self, function, mapper, inputs):
         self.function = function
-        r = re.compile("^[a-zA-Z.]*$")
+        r = re.compile("^[a-zA-Z.\(\)]*$")
         if r.match(mapper):
             self.mapper = mapper
         else: 
             raise Exception("wrong mapper")
-        self.set_inputs(inputs)
-        
+        self.inputs_usr = inputs
+        self.inputs = {}
+        #pdb.set_trace()
+        self._mapper_to_inputs(inputs, mapper)
 
 
-    def set_inputs(self, inputs_dic):
-        self._inputs_list = self.mapper.split(".")
-        if all(key in inputs_dic for key in self._inputs_list):
-            self.inputs = {}
-            for ii in self._inputs_list:
-                
-                self.inputs[ii] = np.array(inputs_dic[ii])
-        else:
+    def _mapper_to_inputs(self, inputs_dic, mapper):
+        inner_brackets = True
+        while inner_brackets:
+            bracket_search = re.search('\([^\(\)]+\)', mapper)
+            if bracket_search:
+                expr_bracket = mapper[bracket_search.start() : bracket_search.end()]
+                broadcasted_list = self._broadcasting_input(expr_bracket[1:-1])
+                mapper = mapper.replace(expr_bracket, str(broadcasted_list))
+            else:
+                inner_brackets = False
+                broadcasted_list = self._broadcasting_input(mapper)
+        #pdb.set_trace()
+        if not all(key in self.inputs for key in broadcasted_list):
             raise Exception("incomplite input")
 
+    def _broadcasting_input(self, expr):
+        if "." in expr:
+            input_list = expr.split(".")
+            # checking if list has exactly 2 elements and no special signs
+            if len(input_list) != 2 or "x" in input_list:
+                raise Exception("wrong mapper, too many .x")
+            else:
+                #pdb.set_trace()
+                if input_list[0][0] == "[" and type(eval(input_list[0])) is list: # the first broadcasting was already done
+                    input_list =  self._broadcasting_list_of_input_dot(input_list, 0)
 
-#    def _get_inputs(self):
-#        return self.inputs
+                elif input_list[1][0] == "[" and type(eval(input_list[1])) is list:
+                    input_list =  self._broadcasting_list_of_input_dot(input_list, 1)
 
-#    def _set_inputs(self, value_dic):
+                else:
+                    self.inputs[input_list[0]], self.inputs[input_list[1]] = \
+                        np.broadcast_arrays(self.inputs_usr[input_list[0]], 
+                                            self.inputs_usr[input_list[1]])
+                #pdb.set_trace()
+                return input_list  # pomyslec czy potrzebne
+
+        elif "x" in expr:
+            input_list = expr.split("x")
+            # checking if list has exactly 2 elements and no special signs  
+            if len(input_list) != 2 or "." in input_list:
+                raise Exception("wrong mapper, too many .x")
+            else:
+                if input_list[0][0] == "[" and type(eval(input_list[0])) is list:
+                    input_list = self._broadcasting_list_of_input_cross(input_list, 0)
+                elif input_list[1][0] == "[" and type(eval(input_list[1])) is list:
+                    input_list = self._broadcasting_list_of_input_cross(input_list, 1)
+                else: #dac spr czy 1d
+                    self.inputs[input_list[0]], self.inputs[input_list[1]] = \
+                        np.broadcast_arrays(self.inputs_usr[input_list[0]], \
+                        np.array(self.inputs_usr[input_list[1]])[np.newaxis].transpose())
+                return input_list  # pomyslec czy potrzebne 
+        else:
+            self.inputs[expr] = np.array(self.inputs_usr[expr])
+            return [expr]  # pomyslec czy potrzebne 
+
+
+    def _broadcasting_list_of_input_dot(self, input_list, ind_list):
+        ind_no_list = [i for i in [0,1] if i != ind_list][0]
+        broadcasted_array = self.inputs[eval(input_list[ind_list])[0]]
+        new_array = np.array(self.inputs_usr[input_list[ind_no_list]])
+        if broadcasted_array.shape == new_array.shape:
+            self.inputs[input_list[ind_no_list]] = new_array
+        elif new_array.shape == (1,):
+            self.inputs[input_list[ind_no_list]] = np.brodcast_to(new_array, broadcasted_array.shape)
+        elif broadcasted_array.shape == (1,):
+            self.inputs[input_list[ind_no_list]] = new_array
+            for key in eval(input_list[ind_list]):
+                self.inputs[key] = np.brodcast_to(self.inputs[key], new_array.shape)
+        else:
+            raise Exception("wrong mapper: can't broadcast")
+
+        return [inp for inp in eval(input_list[ind_list])] + [input_list[ind_no_list]]
+
+
+    def _broadcasting_list_of_input_cross(self, input_list, ind_list):
+        ind_no_list = [i for i in [0,1] if i != ind_list][0]
+
+        broadcasted_array = self.inputs[eval(input_list[ind_list])[0]]
+        new_array = np.array(self.inputs_usr[input_list[ind_no_list]])
+        if broadcasted_array.ndim > 1 or new_array.ndim > 1:
+            raise Exception("cross product requires ndim=1")
         
+        if ind_list == 0:
+            for key in eval(input_list[ind_list]):
+                self.inputs[key], self.inputs[input_list[ind_no_list]] = \
+                    np.broadcast_arrays(self.inputs[key], \
+                                            new_array[np.newaxis].transpose())
+        elif ind_list == 1:
+            for key in eval(input_list[ind_list]):
+                self.inputs[input_list[ind_no_list]], self.inputs[key] = \
+                    np.broadcast_arrays(new_array, \
+                                            self.inputs[key][np.newaxis].transpose())
+
+        #pdb.set_trace()
+        return [inp for inp in eval(input_list[ind_list])] + [input_list[ind_no_list]]
+
 
 
 
@@ -37,8 +120,11 @@ class SNode(object):
         self.output = self.function(**self.inputs)
 
 
+    
 
 
+# nie dziala z dwoma nawiasami po lewej i prawej
+# czy outer produck tylko dla 1d?? tak zakladam
 # sprawdzanie argumentow f-cji i dopasowywanie (na poczatku jako kwrgs)
 # tworzenie tablic a/b w zaleznosci od ./x i podawanie tego do f-cji
 # zrezygnowac z warunku, ze a i b trza w init podawac?
--- test_node.py
+++ test_node.py
@@ -1,18 +1,28 @@
 import pytest, pdb
+import numpy as np
 
 from super_node import SNode
 
 def my_function_1(a):
+    #pdb.set_trace()
     return a**2 - 9
 
 def my_function_2(a, b):
+    #pdb.set_trace()
     return a * b - 9
+
+def my_function_3(a, b, c):
+    return a * b - c
+
+def my_function_4(a, b, c, d):
+    return a * b - c * d
 
 
 def test_single_node_1():
     sn = SNode(function=my_function_1, mapper='a', inputs={"a" : [3, 1, 8]})
     #pdb.set_trace()
     sn.run()
+    pdb.set_trace()
     assert (sn.output == [0, -8, 55]).all()
 
 @pytest.mark.parametrize("inputs_dic, expected_output", [
@@ -23,6 +33,75 @@ def test_single_node_2(inputs_dic, expected_output):
     sn = SNode(function=my_function_2, mapper='a.b', inputs=inputs_dic)
     sn.run()
     assert (sn.output == expected_output).all()
+
+
+@pytest.mark.parametrize("inputs_dic, expected_output", [
+        ({"a":[3, 1], "b":[1, 2]}, np.array([[-6, -8], [-3, -7]])),
+        ])
+def test_single_node_3(inputs_dic, expected_output):
+    sn = SNode(function=my_function_2, mapper='axb', inputs=inputs_dic)
+    sn.run()
+    pdb.set_trace()
+    assert (sn.output == expected_output).all()
+
+@pytest.mark.parametrize("inputs_dic, expected_output", [
+        ({"a":[3, 1], "b":[1, 2], "c":[0, 1]}, [3, 1]),
+        ])
+def test_single_node_4(inputs_dic, expected_output):
+    sn = SNode(function=my_function_3, mapper='(a.b).c', inputs=inputs_dic)
+    sn.run()
+    pdb.set_trace()
+    assert (sn.output == expected_output).all()
+
+@pytest.mark.parametrize("inputs_dic, expected_output", [
+        ({"a":[3, 1], "b":[1, 2], "c":[0, 1]}, [3, 1]),
+        ])
+def test_single_node_5(inputs_dic, expected_output):
+    sn = SNode(function=my_function_3, mapper='a.(b.c)', inputs=inputs_dic)
+    sn.run()
+    pdb.set_trace()
+    assert (sn.output == expected_output).all()
+
+
+@pytest.mark.parametrize("inputs_dic, expected_output", [
+        ({"a":[3, 1], "b":[1, 2], "c": [[1,0],[1,2]]}, np.array([[2, 1], [5, 0]])),
+        ])
+def test_single_node_6(inputs_dic, expected_output):
+    sn = SNode(function=my_function_3, mapper='(axb).c', inputs=inputs_dic)
+    sn.run()
+    pdb.set_trace()
+    assert (sn.output == expected_output).all()
+
+
+@pytest.mark.parametrize("inputs_dic, expected_output", [
+        ({"a":[3, 1], "b":[1, 2], "c": [1,0]}, np.array([[2, 0], [6, 2]])),
+        ])
+def test_single_node_7(inputs_dic, expected_output):
+    sn = SNode(function=my_function_3, mapper='ax(b.c)', inputs=inputs_dic)
+    sn.run()
+    pdb.set_trace()
+    assert (sn.output == expected_output).all()
+
+
+@pytest.mark.parametrize("inputs_dic, expected_output", [
+        ({"a":[3, 1], "b":[1, 2], "c": [1,0]}, np.array([[2, 6], [0, 2]])),
+        ])
+def test_single_node_8(inputs_dic, expected_output):
+    sn = SNode(function=my_function_3, mapper='(b.c)xa', inputs=inputs_dic)
+    sn.run()
+    pdb.set_trace()
+    assert (sn.output == expected_output).all()
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("inputs_dic, expected_output", [
+        ({"a":[3, 1], "b":[1, 2], "c": [1,0], "d": [2,1]}, np.array([[1, 0], [3, 2]])),
+        ])
+def test_single_node_9(inputs_dic, expected_output):
+    sn = SNode(function=my_function_4, mapper='(a.b)x(c.d)', inputs=inputs_dic)
+    sn.run()
+    pdb.set_trace()
+    assert (sn.output == expected_output).all()
+
 
 
 def test_single_node_wrong_mapper():
